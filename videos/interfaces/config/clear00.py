9e4bc120625b8065fb3177ba7864065dd259063e
Ariel <ariel@clear.ml>
2021-02-03 14:57:46 +0200

    working on pilot demo of config smells

--- videos/interfaces/config/clear00.py
+++ videos/interfaces/config/clear00.py
@@ -1,26 +1,6 @@
 from clearml import Task
 from argparse import ArgumentParser
 
-import pandas as pd
-from pandas import util as pdutil
-
-# but I want to add an experimental feature!
-IS_ACTIVATED = True
-THRESH = 0
-COLUMN = "A"
-
-
-def experimental_new_feature(data: pd.DataFrame):
-    print(f"is actually activated {IS_ACTIVATED}")
-    print(f"Column: {COLUMN}")
-    print(f"Thresh: {THRESH}")
-    if not IS_ACTIVATED:
-        return
-    else:
-        data[data[COLUMN] <= THRESH] = pd.NA
-        return data
-
-
 # need to define this to do validation on str to bool
 def boolean_string(s):
     if s not in {'False', 'True'}:
@@ -30,9 +10,13 @@ def boolean_string(s):
 
 # parser from zylo117/Yet-Another-EfficientDet-Pytorch
 parser = ArgumentParser('Yet Another EfficientDet Pytorch: SOTA object detection network - Zylo117')
-parser.add_argument('-p', '--project', type=str, default='coco', help='project file that contains parameters')
-parser.add_argument('-c', '--compound_coef', type=int, default=0, help='coefficients of efficientdet')
-parser.add_argument('-n', '--num_workers', type=int, default=12, help='num_workers of dataloader')
+parser.add_argument('--debug', type=boolean_string, default=False,
+                    help='whether visualize the predicted boxes of training, '
+                         'the output images will be in test/')
+parser.add_argument('--data_path', type=str, default='datasets/', help='the root folder of dataset')
+parser.add_argument('--log_path', type=str, default='logs/')
+parser.add_argument('--saved_path', type=str, default='logs/')
+
 parser.add_argument('--batch_size', type=int, default=12, help='The number of images per batch among all devices')
 parser.add_argument('--head_only', type=boolean_string, default=False,
                     help='whether finetunes only the regressor and the classifier, '
@@ -41,34 +25,37 @@ parser.add_argument('--lr', type=float, default=1e-4)
 parser.add_argument('--optim', type=str, default='adamw', help='select optimizer for training, '
                                                                'suggest using \'admaw\' until the'
                                                                ' very final stage then switch to \'sgd\'')
-parser.add_argument('--num_epochs', type=int, default=500)
-parser.add_argument('--val_interval', type=int, default=1, help='Number of epoches between valing phases')
-parser.add_argument('--save_interval', type=int, default=500, help='Number of steps between saving')
+parser2 = ArgumentParser('more options')
+parser2.add_argument('--num_epochs', type=int, default=500)
+parser2.add_argument('--val_interval', type=int, default=1, help='Number of epoches between valing phases')
+parser2.add_argument('--save_interval', type=int, default=500, help='Number of steps between saving')
+
 parser.add_argument('--es_min_delta', type=float, default=0.0,
                     help='Early stopping\'s parameter: minimum change loss to qualify as an improvement')
 parser.add_argument('--es_patience', type=int, default=0,
                     help='Early stopping\'s parameter: number of epochs with no improvement after which training will be stopped. Set to 0 to disable this technique.')
-parser.add_argument('--data_path', type=str, default='datasets/', help='the root folder of dataset')
-parser.add_argument('--log_path', type=str, default='logs/')
-parser.add_argument('-w', '--load_weights', type=str, default=None,
+
+parser3 = ArgumentParser('file options')
+parser3.add_argument('-w', '--load_weights', type=str, default=None,
                     help='whether to load weights from a checkpoint, set None to initialize, set \'last\' to load last checkpoint')
-parser.add_argument('--saved_path', type=str, default='logs/')
-parser.add_argument('--debug', type=boolean_string, default=False,
-                    help='whether visualize the predicted boxes of training, '
-                         'the output images will be in test/')
+parser3.add_argument('-p', '--project', type=str, default='coco', help='project file that contains parameters')
+parser3.add_argument('-c', '--compound_coef', type=int, default=0, help='coefficients of efficientdet')
+parser3.add_argument('-n', '--num_workers', type=int, default=12, help='num_workers of dataloader')
 
 if __name__ == "__main__":
     # this will create "ARGS" in the UI
-    task = Task.init(project_name='GOOD THING BAD THING',
-                     task_name='good thing bad thing 1')
+    task = Task.init(project_name='CLEARML AS GLASS',
+                     task_name='too many options 2',
+                     auto_connect_arg_parser=False,
+                     )
 
     args = parser.parse_args()
+    arg2 = parser2.parse_args()
+    arg3 = parser3.parse_args()
+    task.connect(args, 'main')
+    task.connect(arg2, 'more')
+    task.connect(arg3, 'snore')
 
-    # random data
-    data = pdutil.testing.makeDataFrame()
-    task.register_artifact('demo', data)
-    data = experimental_new_feature(data)
-    data.dropna(inplace=True)
 
     ...
     # actual experiment...
