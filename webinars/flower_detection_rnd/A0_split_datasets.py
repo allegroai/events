edab583df07b3ddcbf12650c5c2b4c5e3450f4fc
Ariel <ariel@clear.ml>
2021-02-21 09:59:01 +0200

    dataset split and create

--- webinars/flower_detection_rnd/A0_split_datasets.py
+++ webinars/flower_detection_rnd/A0_split_datasets.py
@@ -1,38 +1,47 @@
 from clearml import Task, Dataset
 from dataclasses import dataclass
+from pathlib import Path
+
+DELETE_DATASETS_IF_ALREADY_EXIST = True
 
 
-def extract_relevant_filenames(dataset_path, image_size):
+@dataclass
+class DataSplitConf:
+    # already exists - "someone already uploaded via cli"
+    input_dataset_id: str = "86895530658c47a4918bda4f0d92c3e8"
+    image_size_values: set = (192, 224, 331, 512)
+    dataset_name: str = "flower_detection"
+    folder_name_prefix = "jpeg-"
+
+
+def extract_relevant_filenames(dataset_path, image_size, folder_name_pattern=None):
+    # original dataset glob patterns
     import os
     import glob
+    folder_name_pattern = f"jpeg-{image_size}x{image_size}" if\
+        folder_name_pattern is None else folder_name_pattern
     train_image_paths = glob.glob(
         os.path.join(
-            dataset_path, f"jpeg-{image_size}x{image_size}", "train", "**", "*.jpeg"
+            dataset_path, folder_name_pattern, "train", "**", "*.jpeg"
         ),
         recursive=True,
     )
 
     valid_image_paths = glob.glob(
         os.path.join(
-            dataset_path, f"jpeg-{image_size}x{image_size}", "val", "**", "*.jpeg"
+            dataset_path, folder_name_pattern, "val", "**", "*.jpeg"
         ),
         recursive=True,
     )
     return train_image_paths, valid_image_paths
 
-@dataclass
-class DataSplitConf:
-    input_dataset_id: str = "86895530658c47a4918bda4f0d92c3e8"
-    image_size_values: set = (192, 224, 331, 512)
-    dataset_name: str = "flower_detection"
-
-
 
 if __name__ == '__main__':
+    project_name = 'R|D?R&D! Webinar 01'
     task = Task.init(
-        project_name='R|D?R&D! Webinar 01',
+        project_name=project_name,
         task_name='dataset split to sizes',
-        output_uri=True,  # auto save everything to Clearml Free
+        output_uri=True,  # auto save everything to ClearML Free
         task_type=Task.TaskTypes.data_processing
     )
 
@@ -43,23 +52,53 @@ if __name__ == '__main__':
     input_dataset_folder = input_dataset.get_local_copy()
 
     for image_size in cfg.image_size_values:
-        # if dataset exists skip creatino
-
-        test_if_exists = Dataset.get()
-
-        # test that there are files train/val
-
+        # if dataset exists skip creating
         dataset_name = f"{cfg.dataset_name}_{image_size}x{image_size}_"
 
-        new_dataset_train = Dataset.create(
-            dataset_name=dataset_name+"train",
-            dataset_project=task.project,
-            parent_datasets=[cfg.input_dataset_id]
-        )
-        new_dataset_val = Dataset.create(
-            dataset_name=dataset_name+"val",
-            dataset_project=task.project,
-            parent_datasets=[cfg.input_dataset_id]
-        )
+        try:
+            test_if_exists = Dataset.list_datasets(
+                dataset_project=project_name,
+                partial_name=dataset_name,
+                only_completed=True,
+            )
+
+            if len(test_if_exists):
+                if DELETE_DATASETS_IF_ALREADY_EXIST:
+                    print(f'found datasets in the project with image size {image_size}')
+                    for t in test_if_exists:
+                        try:
+                            Dataset.delete(t)
+                        except ValueError:
+                            print(f'could not delete {t} - has children?')
+                else:
+                    continue
+
+        except ValueError:
+            print(f'Did not find {dataset_name}, creating!')
+
+        train_files, validation_files = \
+            extract_relevant_filenames(input_dataset_folder, image_size)
+
+        # test that there are files train/val
+        if not len(train_files):
+            raise ValueError(f'No files found for image size {image_size} on folder {input_dataset_folder}')
+        if not len(validation_files):
+            raise NotImplementedError('No validation files - option for train only not supported')
+
+        # train
+        path_for_image_size = Path(input_dataset_folder) / (cfg.folder_name_prefix + f"{image_size}x{image_size}")
+        for stage in ['train', 'val']:  # TODO 'test'
+            file_folder = path_for_image_size / stage
+
+            new_dataset = Dataset.create(
+                dataset_name=dataset_name+stage,
+                dataset_project=project_name,
+                parent_datasets=[cfg.input_dataset_id]
+            )
+
+            new_dataset.add_files(file_folder, wildcard='*.jp*g')
+            new_dataset.upload(show_progress=True, verbose=True)
+            new_dataset.finalize()
+            new_dataset.publish()
 
 
